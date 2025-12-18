f9dee2dce225ffe524f22da8924e4d7e4b68c3c9
Mateojami10 <majamic@uce.edu.ec>
2025-12-18 17:04:48 -0500

    correcto

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
+  <div id="app">
+    <JuegoCasinoView/>
+  </div>
   
 </template>
 
 <script>
+import JuegoCasinoView from './views/JuegoCasinoView.vue';
 export default {
+  name: 'App',
+  components: {
+    JuegoCasinoView
+  }
 
 }
 </script>
---
+++ src/views/JuegoCasinoView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="view-container">
+        <JuegoCasino/>
+    </div>
+  
+</template>
+
+<script>
+import JuegoCasino from '@/components/JuegoCasino.vue';
+
+export default {
+    components: {
+        JuegoCasino
+    }
+}
+</script>
+
+<style scoped>
+
+.view-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 2px solid black;
+   
+}
+
+</style>
